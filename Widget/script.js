d5401fc6871b38c435fab59f8a251f4a00bf9c3b
Andrey Hristoforov <andrey.hristoforov@gmail.com>
2022-05-23 15:07:10 +0400

    Реализовал стоблцы. Немного описал класс виджет. Реализовал драгндроп, но он баганный(можно один виджет запихнуть в другой).

--- Widget/script.js
+++ Widget/script.js
@@ -1,66 +1,90 @@
-const galeryFrame = document.getElementById("galeryFrame");
-const galeryIndicators = document.getElementById("galeryIndicators");
-const closeRegistrationFormButton = document.getElementById("closeRegistrationFormButton");
-const openRegistrationDialog = document.getElementById("openRegistrationDialog");
-const registrationSubmitButton = document.getElementById("registrationSubmitButton");
+AddEventListeners();
 
-galeryFrame.addEventListener("click", rotatePics);
-closeRegistrationFormButton.addEventListener("click", HideRegistrationForm);
-openRegistrationDialog.addEventListener("click", ShowRegistrationForm);
-registrationSubmitButton.addEventListener("click", registrationSubmitAction);
 
-PlacingIndicators();
 
-function rotatePics()
-{
-   for (var i = 0; i < galeryFrame.children.length; i++){
-      if (galeryFrame.children[i].className == "galery-frameItem-visible")
-      {
-         galeryFrame.children[i].className
-            = "galery-frameItem-invisible";
-         galeryFrame.children[(i + 1) % galeryFrame.children.length].className
-            = "galery-frameItem-visible";
+function AddEventListeners(){
+   let widgetColumns = document.querySelectorAll(".widgetColumnt");
+   widgetColumns.forEach(element => {
+      let addButton = element.lastElementChild;
+      addButton.addEventListener("click", ShowChoosingWidgetDialog);
 
-         switchIndicators((i + 1) % galeryFrame.children.length);
-         break;
-      }
-   };
-};
-
-function PlacingIndicators(){
-   for (var i = 0; i < galeryFrame.children.length; i++){
-      let span = document.createElement("span");
-      span.className = "galery-indicatorsItem";
-      if (galeryFrame.children[i].className == "galery-frameItem-visible"){
-         span.classList.add("selected");
-      }
-      galeryIndicators.appendChild(span);
-   }
-};
-
-function switchIndicators(index){
-   for (var i = 0; i < galeryIndicators.children.length; i++){
-      galeryIndicators.children[i].className = "galery-indicatorsItem";
-   }
-   galeryIndicators.children[index].classList.add("selected");
-};
-
-function ShowRegistrationForm(){
-   let div = document.querySelector(".registrationForm");
-   div.style.display = "flex";
-};
-
-function HideRegistrationForm(){
-   let div = document.querySelector(".registrationForm");
-   div.style.display = "none";
-};
-
-function registrationSubmitAction(event){
-   event.preventDefault();
-   HideRegistrationForm();
-   let fullName = document.getElementById("fullNameInput").innerHTML;
-   let email = document.getElementById("emailInput").innerHTML;
-
-   document.getElementById("FullNameTableCell").innerHTML = fullName;
-   document.getElementById("EmailTableCell").innerHTML = fullName;
+      element.firstElementChild.addEventListener('dragover', WidgetColunmDragoveredHandler);
+      element.firstElementChild.addEventListener('dragleave', WidgetColunmDragleavedHandler);
+   });
 }
+
+function WidgetColunmDragoveredHandler(event)
+{
+   event.preventDefault();
+   if(dragged.parentElement === event.target.parentElement)
+      return;
+   dragovered = event.target;
+}
+
+function WidgetColunmDragleavedHandler(event){
+   dragovered = undefined;
+}
+
+let dragged = undefined;
+let dragovered = undefined;
+
+
+function ShowChoosingWidgetDialog(event){
+   let dialog = document.querySelector(".choosingWidgetDialog");
+   dialog.style.display = "flex";
+}
+
+class Widget{
+   constructor(bgcolor, tcolor, labelText){
+      this.bgcolor = bgcolor;
+      this.tcolor = tcolor;
+      this.Shell = document.createElement("div");
+      this.Shell.classList.add("widget");
+      this.Shell.style.color = tcolor;
+      this.Shell.style.background = bgcolor;
+      this.Label = document.createElement("h2");
+
+      this.Shell.addEventListener('dragstart', WidgetDragStartHandler);
+      this.Shell.addEventListener('dragend', WidgetDragEndHandler);
+
+      this.Label.innerHTML = labelText;
+   }
+   Insert(parent){
+      parent.appendChild(this.ToHTMLElement());
+   }
+
+   ToHTMLElement(){
+      this.Shell.appendChild(this.Label);
+
+      this.Shell.draggable = true;
+
+      return this.Shell;
+   }
+}
+
+function WidgetDragStartHandler(event){
+   dragged = event.target;
+}
+function WidgetDragEndHandler(event){
+   if(dragged != undefined && dragovered != undefined)
+   {
+      dragged.parentElement.removeChild(dragged);
+      dragovered.appendChild(dragged);
+
+      console.log(dragovered);
+
+      return;
+   }
+
+   dragged = undefined;
+   dragovered = undefined;
+   return;
+}
+
+const firstColumn = document.querySelector(".widgetColumnt").firstElementChild;
+let w = new Widget('gray', 'white', 'Widget');
+w.Insert(firstColumn);
+let w2 = new Widget('blue', 'white', 'another');
+w2.Insert(firstColumn);
+let w3 = new Widget('gray', 'white', 'Widget');
+w3.Insert(firstColumn);
